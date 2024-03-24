d1dfa1c51823df40f23c5c52c8c81e156a3a8b6c
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-24 12:01:02 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
+import Home from "./Pages/Home"
 
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      {/* <button className="btn">Button</button> */}
-      <button className="btn">Button</button>
-    </h1>
+      <Home></Home>
     </>
   )
 }
