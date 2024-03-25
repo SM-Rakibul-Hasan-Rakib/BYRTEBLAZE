528ac7d13e8f7ed0e2ee6e5c778ba265abc386cd
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-25 17:32:04 +0600

    Blog done

--- src/main.jsx
+++ src/main.jsx
@@ -5,39 +5,44 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // import Home from "./Pages/Home.jsx";
 import Blogs from "./Pages/Blogs.jsx";
+import Blog from "./Pages/Blog.jsx";
 import Bookmark from "./Pages/Bookmark.jsx";
 import MainLayout from "./Layouts/MainLayout.jsx";
 import Home from "./Pages/Home.jsx";
 
-
- const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path: '/',
-    element:<MainLayout></MainLayout>,
+    path: "/",
+    element: <MainLayout></MainLayout>,
     children: [
       {
-        path: '/',
-        element:<Home></Home>
+        path: "/",
+        element: <Home></Home>,
       },
       {
-        path:'/blogs',
+        path: "/blogs",
         // element: <App></App>
         element: <Blogs></Blogs>,
-        loader: () => fetch('https://dev.to/api/articles?per_page=20&top=7')
+        loader: () => fetch("https://dev.to/api/articles?per_page=20&top=7"),
       },
       {
-        path:'/bookmarks',
-        // element: <App></App>
-        element: <Bookmark></Bookmark>
+        path: "/blog/:id",
+        element: <Blog></Blog>,
+        loader: ({ params }) =>fetch(`https://dev.to/api/articles/${params.id}`)
+         
       },
-    ]
-  
+      {
+        path: "/bookmarks",
+        // element: <App></App>
+        element: <Bookmark></Bookmark>,
+      },
+    ],
   },
-  
- ])
+]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(<>
-{/* <App /> */}
- <RouterProvider router={router}></RouterProvider>
-</>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
+  </>
 );
